9062290f74a81a330dca173c2695b7b18ac57f0a
Irwin <irgranados16@gmail.com>
2020-11-16 21:49:49 -0600

    deploying

---
+++ client/build/precache-manifest.a69cbedf5ee1dc299e4797b20b71461b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f0b59063e17c3e342dbbc365042c8f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58ddc197e2c323aeb19f",
+    "url": "/static/css/main.997d4878.chunk.css"
+  },
+  {
+    "revision": "5309e449abb858676493",
+    "url": "/static/js/2.7f4868fe.chunk.js"
+  },
+  {
+    "revision": "223be75f6809993b56a94be00d710bed",
+    "url": "/static/js/2.7f4868fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58ddc197e2c323aeb19f",
+    "url": "/static/js/main.5fc78d7d.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
